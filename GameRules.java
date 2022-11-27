f9b5c416a77d2bd85508ff8fa3ef5fcc24389782
Robertas Petrokas <robertas.petrokas@devbridge.com>
2022-11-27 21:38:07 +0200

    created gamerules class

---
+++ GameRules.java
@@ -0,0 +1,90 @@
+package main;
+import main.Interfaces.IBoard;
+import main.Interfaces.IGameRules;
+import main.tetriminoes.*;
+import java.util.ArrayList;
+public class GameRules implements IGameRules {
+    private final IBoard lenta;
+    private final int tetrimino;
+    public GameRules(IBoard lenta) {
+        this.board = lenta;
+    }
+    // inits random tetrimino
+    public Tetrimino initRandomTetrimino() {
+        switch (TetriminoType.getRandomTetrimino().ordinal()) {
+            case 0 -> {
+                return new TetriminoLine(this);
+            }
+            case 1 -> {
+                  return new TetriminoL(this);
+            }
+            case 2 -> {
+              return new TetriminoJ(this);
+            }
+            case 3 -> {
+                return new TetriminoSquare(this);
+            }
+
+            case 4 -> {
+                return new TetriminoS(this);
+            }
+            case 5 -> {
+                    return new TetriminoT(this);
+            }
+
+            case 6 -> {
+                return new TetriminoZ(this);
+            }
+        }
+        return null;
+    }
+    public void processusercommand(Tetrimino tetrimino, int userCommand) {
+        if (userCommand == 's')
+               tetrimino.moveDown();
+        if (userCommand == 'a')
+            tetrimino.moveLeft();
+        if (userCommand == 'd')
+              tetrimino.moveRight();
+        if (userCommand == 'q')
+              tetrimino.rotateLeft();
+        if (userCommand == 'e')
+                tetrimino.rotateRight();
+    }
+    public boolean HasFallen(Tetrimino tetrimino) {
+        for (Coordinates coordinate : tetrimino.getCoordinates()) {
+            if (coordinate.getX() == board.getBoardHeight() - 1 || board.getNextElement(coordinate) == TetriminoType.tetriminoFallen) {
+                    tetrimino.setFallen();
+                return true;
+            }
+        }
+        return false;
+    }
+    public boolean move(ArrayList<Coordinates> coordinates, int x, int y) {
+        for (Coordinates coordinate : coordinates) {
+            if (coordinate.getX() + x >= board.getBoardHeight() || coordinate.getY() + y < 0 || coordinate.getY() + y >= board.getBoardWidth() || board.getElement(new Coordinates(coordinate.getX() + x, coordinate.getY() + y)) != null) {
+                  return false;
+            }
+        }
+        return true;
+    }
+    public boolean canRotate(Tetrimino tetrimino) {
+        ArrayList<Coordinates> Coordinates = tetrimino.getCoordinates();
+        int[][][] BlockCoords = tetrimino.getRotationCoords();
+            int Orientation = tetrimino.getOrientation();
+        int HighestX = tetrimino.getHighestX(coordinates);
+           int LowestY = tetrimino.getLowestY(coordinates);
+           boolean returnanswer;
+        for (int i = 0; i < Coordinates.size(); i++) {
+            int blockX = BlockCoords[Orientation][i][0];
+             int blockY = BlockCoords[Orientation][i][1];
+              if (blockX + HighestX >= board.getBoardHeight() || blockY + LowestY >= board.getBoardWidth()) {
+                returnanswer = false;
+            }
+            if (board.getElement(new Coordinates(blockX + highestX, blockY + LowestY)) != null) {
+                returnanswer= false;
+             }
+        }
+          returnanswer=true;
+          return returnanswer;
+    }
+}
